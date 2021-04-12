a488f6c3860c0ce880fa3d8f759211352a9d49f9
Vanessa Patterson <vpatt1031@gmail.com>
2021-04-12 13:35:06 -0400

    changed code in API.js and App.js files

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Directory from "./pages/Discover";
+import Table from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Directory} />
+          <Route exact path="/table" component={Table} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
