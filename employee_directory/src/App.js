65342521c247447d55284f230b8446c3a6011e44
Vanessa Patterson <vpatt1031@gmail.com>
2021-04-13 13:30:07 -0400

    replaced src file with new code to render a table

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,24 +1,79 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Directory from "./pages/Discover";
-import Table from "./pages/About";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Directory} />
-          <Route exact path="/table" component={Table} />
-        </Wrapper>
-        <Footer />
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      isLoading: false,
+      isError: false
+    }
+  }
+
+  //async function get request
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  
+    if (response.ok) {
+      const users = await response.json()
+      this.setState({ users, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
+    }
+  }
+
+  renderTableHeader = () => {
+    return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr.toUpperCase()}</th>)
+  }
+
+  renderTableRows = () => {
+    return this.state.users.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.username}</td>
+          <td>{user.email}</td>
+          <td>{`${user.address.street}, ${user.address.city}`}</td>
+          <td>{user.phone}</td>
+          <td>{user.website}</td>
+          <td>{user.company.name}</td>
+        </tr>
+      )
+    })
+  }
+  
+  render () {
+    const {users, isLoading, isError} = this.state
+
+    if (isLoading){
+      return <div>Loading...</div>
+    }
+
+    if(isError){
+      return <div>Error...</div>
+    }
+
+    return users.length > 0
+      ? (
+        <table>
+          <thead>
+            <tr>
+              {this.renderTableHeader()}
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableRows()}
+          </tbody>
+        </table>
+      ) : (
+        <div>
+          No users.
       </div>
-    </Router>
-  );
+      )
+    
+  }
 }
 
 export default App;
